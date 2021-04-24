5393a166067488f4bd1734ad351abdd271375b32
Psom9 <75801155+Psom9@users.noreply.github.com>
2021-04-24 18:50:34 +0200

    update list

--- app/controllers/cat.controller.js
+++ app/controllers/cat.controller.js
@@ -134,7 +134,7 @@ exports.count = (req, res) => {
 // Get list of cat names
 exports.getList = (req, res) => {
     Cat.find ({})
-        .then(cat => {
+        .toArray(cat => {
             res.send(cat.name);})
         .catch(err => res.status(500).json({message: err}));
 }
